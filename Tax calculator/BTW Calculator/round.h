312af9d37c629f48ab876e7d4ff03cdb261a5869
RIPP3R1337 <136556432+RIPP3R1337@users.noreply.github.com>
2023-06-15 00:00:37 +0200

    Delete Tax calculator directory

--- Tax calculator/BTW Calculator/round.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-// function which rounds a float to two decimal places
-float roundToTwoDecimalPlaces(float value) {
-	return std::round(value * 100) / 100;
-}
-
