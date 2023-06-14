5dacdfc80213c631c8a489530ee7034f63a13b66
RIPP3R1337 <136556432+RIPP3R1337@users.noreply.github.com>
2023-06-15 00:21:27 +0200

    Update Source.cpp
    Made it english

--- Source.cpp
+++ Source.cpp
@@ -23,20 +23,20 @@ int main()
 		double prijs;
 		char omrekenen;
 
-		std::cout << "Wilt u inclusief of exclusief berekenen? (I/E): ";
+		std::cout << "Would you like to include or exclude tax? (I/E): ";
 		std::cin >> omrekenen;
 		// convert to lowercase, and then convert back to a char type (as the tolower() function returns an integer)
 		omrekenen = char(tolower(omrekenen));
 		// check that the user entered i or e
 		if (omrekenen != 'i' && omrekenen != 'e') {
-			std::cout << "Voer alstublieft I/E in. U voerde in: " << omrekenen << "." << std::endl;
+			std::cout << "Please enter I/E. You entered: " << omrekenen << "." << std::endl;
 		}
 
-		std::cout << "Voer uw bedrag in: ";
+		std::cout << "Enter your amount € ";
 		std::cin >> prijs;
 		// check if prijs is a valid positive number
 		if (!isPositiveDouble(prijs)) {
-			std::cout << "Voer alstublieft een positief nummer in. U antwoordde: " << prijs << "." << std::endl;
+			std::cout << "Please enter a positive number. You entered: " << prijs << "." << std::endl;
 		}
 
 #ifdef _WIN32
@@ -48,22 +48,22 @@ int main()
 		switch (omrekenen)
 		{ // since we converted omrekenen to be lowercase no matter what, we don't need a separate case for 'e' and 'E', etc
 		case 'e':
-			std::cout << "U heeft " << prijs << " Euro ingevoerd\n\n";
-			std::cout << "21% BTW = +" << roundToTwoDecimalPlaces(prijs * 21 / 100) << " Euro\n\n";
-			std::cout << "Dit bedrag is: " << roundToTwoDecimalPlaces(prijs * 1.21) << " Euro inclusief BTW\n\n";
+			std::cout << "You entered " << prijs << " Euro\n\n";
+			std::cout << "21% TAX = +" << roundToTwoDecimalPlaces(prijs * 21 / 100) << " Euro\n\n";
+			std::cout << "This amount is: " << roundToTwoDecimalPlaces(prijs * 1.21) << " Euro including TAX\n\n";
 			break;
 		case 'i':
-			std::cout << "U heeft " << prijs << " Euro ingevoerd\n\n";
-			std::cout << "21% BTW = -" << roundToTwoDecimalPlaces(prijs * 21 / 121) << " Euro\n\n";
-			std::cout << "Dit bedrag is: " << roundToTwoDecimalPlaces(prijs / 1.21) << " Euro Exclusief BTW\n\n";
+			std::cout << "You entered " << prijs << " Euro\n\n";
+			std::cout << "21% TAX = -" << roundToTwoDecimalPlaces(prijs * 21 / 121) << " Euro\n\n";
+			std::cout << "This amount is: " << roundToTwoDecimalPlaces(prijs / 1.21) << " Euro excluding TAX\n\n";
 			break;
 		default:
-			std::cout << "[ERROR] Geen geldige prijs of er is een ander fout opgetreden!\n";
+			std::cout << "[ERROR] Not a valid value or another error has occured!\n";
 			
 		}
 
 
-		std::cout << "\n\nWilt u een nieuw bedrag berekenen? (Y/N): ";
+		std::cout << "\n\nWould you like to calculate another price? (Y/N): ";
 		std::cin >> restart;
 
 	} while (tolower(restart) == 'y');
